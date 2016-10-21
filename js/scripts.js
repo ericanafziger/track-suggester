81fd8edeee73cb7cbbbd79d2a59d394220c56138
Erica Nafziger <garbererica@gmail.com>
2016-10-21 09:15:11 -0700

    add radio buttons for front-end or back-end tracks

--- js/scripts.js
+++ js/scripts.js
@@ -2,17 +2,24 @@ $(document).ready(function(){
   $("#blanks form").submit(function(event){
     var companyInput = parseInt($("#company").val());
     var interestsInput = parseInt($("#interests").val());
-console.log(companyInput);
+    var nameInput = $("input#name").val();
+    var emailInput = $("input#email").val();
+    var addressInput = $("input#address").val();
+    var commentsInput = $("input#comments").val();
+    var styleInput = parseInt($("input:radio[name=style]:checked").val());
 
-    if (companyInput + interestsInput === 8) {
+    $(".name").text(nameInput);
+    $(".email").text(emailInput);
+
+    if (companyInput + interestsInput + styleInput === 18) {
     $("#ruby").show();
-  } else if (companyInput + interestsInput === 3) {
+  } else if (companyInput + interestsInput + styleInput === 13) {
     $("#csharp").show();
-  } else if (companyInput + interestsInput === 4) {
+  } else if (companyInput + interestsInput + styleInput === 14) {
     $("#java").show();
-  } else if (companyInput + interestsInput === 6) {
+  } else if (companyInput + interestsInput + styleInput === 16) {
     $("#php").show();
-  } else if (companyInput + interestsInput === 10) {
+  } else if (companyInput + interestsInput + styleInput === 30) {
     $("#design").show();
   }
 
