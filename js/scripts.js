ad87b337fc486fdcfb6bbf3082426f24a853301c
Erica Nafziger <garbererica@gmail.com>
2016-10-21 08:46:09 -0700

    add ruby c#, java selectors with companyInput and interestsInput

--- js/scripts.js
+++ js/scripts.js
@@ -2,10 +2,15 @@ $(document).ready(function(){
   $("#blanks form").submit(function(event){
     var companyInput = parseInt($("#company").val());
     var interestsInput = parseInt($("#interests").val());
+console.log(companyInput);
 
-
-    if (companyInput + interestsInput === 8)
+    if (companyInput + interestsInput === 8) {
     $("#ruby").show();
+  } else if (companyInput + interestsInput === 3) {
+    $("#csharp").show();
+  } else if (companyInput + interestsInput === 4) {
+    $("#java").show();
+  }
 
     event.preventDefault();
 
